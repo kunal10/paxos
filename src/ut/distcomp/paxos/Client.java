461fb6ba3ee58d6e0ae1474e68b69c1c1a1d3a57
Ashwini Venkatesh <asvenk@TACC-SpareMBP15.local>
2015-11-08 17:15:48 -0600

    Terminating changes

--- src/ut/distcomp/paxos/Client.java
+++ src/ut/distcomp/paxos/Client.java
@@ -45,6 +45,7 @@ public class Client {
 	}
 	
 	public void CrashClient(){
+		nc.shutdown();
 		if(receiveThread != null){
 			receiveThread.stop();
 		}
@@ -175,8 +176,7 @@ public class Client {
 	}
 
 	/**
-	 * Chat log of this client received from the servers. TODO: Change it to
-	 * some different structure.
+	 * Chat log of this client received from the servers. 
 	 */
 	private SortedMap<Integer, Command> chatLog;
 	/**
